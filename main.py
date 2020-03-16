f0337a2931470967011b2aba054ac080f3449287
badboy99tw <taiwan.badboy99@gmail.com>
2020-03-16 15:30:47 +0800

    Add answer for Q2

--- main.py
+++ main.py
@@ -22,6 +22,29 @@ def reverse_sentence(sentence):
     return reversed_sentence
 
 
+# Q2
+def count_numbers(end_number):
+    """
+    請寫一個程式，Input 是一個數字，Output 是從 1 到這個數字，
+    扣除掉所有 3 的倍數以及 5 的倍數，但是需要保留同時是 3 和 5 的倍數的總數字數。
+
+    >>> count_numbers(15)
+    9
+    """
+
+    def is_matched(n):
+        if n % 15 == 0:
+            return True
+        elif n % 3 == 0:
+            return False
+        elif n % 5 == 0:
+            return False
+        return True
+
+    numbers_matched = filter(is_matched, range(1, end_number + 1))
+    return sum(1 for _ in numbers_matched)
+
+
 if __name__ == "__main__":
     import doctest
 
